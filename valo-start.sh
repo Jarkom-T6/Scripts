69394d25352316fe01aa683ad7bd82b9bc2909d2
Syakhisk <mail.syakhisk@gmail.com>
2021-11-08 19:45:23 +0700

    add skypie

--- valo-start.sh
+++ valo-start.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-# Written by m42nk
+# Written by jarkom t6
 
 set -eo pipefail
 # Variables
@@ -106,16 +106,16 @@ options {
 };
 eof
 
-mkdir -p /etc/bind/jarkom/
+	mkdir -p /etc/bind/jarkom/
 
-    cat >/etc/bind/named.conf.local <<eof
+	cat >/etc/bind/named.conf.local <<eof
 zone "jualbelikapal.ti6.com" {
   type master;
   file "/etc/bind/jarkom/jualbelikapal.ti6.com";
 };
 eof
-    
-        cat >/etc/bind/jarkom/jualbelikapal.ti6.com <<eof
+
+	cat >/etc/bind/jarkom/jualbelikapal.ti6.com <<eof
 \$TTL    604800
 @       IN      SOA     jualbelikapal.ti6.com. root.jualbelikapal.ti6.com. (
                               2         ; Serial
@@ -127,12 +127,36 @@ eof
 @       IN      NS      jualbelikapal.ti6.com.
 @       IN      A       192.214.2.3
 eof
-        
-  service bind9 restart
+
+	service bind9 restart
 }
 
 function Skypie {
-	echo "halo"
+	apt install \
+		apache2 \
+		php -y
+
+	curl -Lk https://github.com/FeinardSlim/Praktikum-Modul-2-Jarkom/raw/main/super.franky.zip -o super.franky.zip
+	unzip super.franky.zip
+
+	mv super.franky /var/www/super.franky.ti6.com
+
+	cat >/etc/apache2/sites-available/000-default.conf <<eof
+<VirtualHost *:80>
+        ServerAdmin webmaster@localhost
+        ServerName super.franky.ti6.com
+        ServerAlias www.super.franky.ti6.com
+        DocumentRoot /var/www/super.franky.ti6.com
+
+         <Directory /var/www/super.franky.ti6.com>
+             Options +FollowSymLinks -Multiviews
+             AllowOverride All
+         </Directory>
+
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+eof
 }
 
 function TottoLand {
@@ -140,15 +164,15 @@ function TottoLand {
 }
 
 function Water7 {
-  apt update
-  apt install \
-      squid \
-      apache2-utils \
-      -y
+	apt update
+	apt install \
+		squid \
+		apache2-utils \
+		-y
 
-  touch /etc/squid/passwd
-  htpasswd -mb /etc/squid/passwd luffybelikapalti6 luffy_ti6
-  htpasswd -mb /etc/squid/passwd zorobelikapalti6 zoro_ti6
+	touch /etc/squid/passwd
+	htpasswd -mb /etc/squid/passwd luffybelikapalti6 luffy_ti6
+	htpasswd -mb /etc/squid/passwd zorobelikapalti6 zoro_ti6
 
 	cat >/etc/squid/squid.conf <<eof
 include /etc/squid/acl.conf
@@ -169,6 +193,11 @@ http_access allow acltime3
 
 http_access deny all
 http_access allow USERS
+
+acl lan src 192.214.1.0/24 192.214.3.0/24
+acl badsites dstdomain .google.com
+deny_info http://super.franky.ti6.com/ lan
+http_reply_access deny badsites lan
 eof
 
 	cat >/etc/squid/acl.conf <<eof
@@ -184,7 +213,8 @@ eof
 function Loguetown {
 	apt update
 	apt install lynx -y
-  export http_proxy="http://jualbelikapal.ti6.com:5000"
+	export http_proxy="http://jualbelikapal.ti6.com:5000"
+	bash
 }
 
 function Alabasta {
